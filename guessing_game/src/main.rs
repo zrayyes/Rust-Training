9554ff5f18c438513c3f5d594cd53eb197cbd8d8
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-08 11:14:57 +0200

    Added a compare

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,8 @@
 extern crate rand;
 
-// Input/Output library
 use rand::Rng;
-use std::io;
+use std::cmp::Ordering;
+use std::io; // Input/Output library
 
 fn main() {
     println!("Guess the number!");
@@ -11,7 +11,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("{}", secret_number);
+    println!("Secret number: {}", secret_number);
 
     // mut = make a variable mutable
     let mut guess = String::new();
@@ -20,5 +20,17 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // shadows the variable guess
+    // .trim removes trailing whitespaces
+    // .parse parses the string as a numerical value
+    // .expect throws an error if something goes wrong
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
