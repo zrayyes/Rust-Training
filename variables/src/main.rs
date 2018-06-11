fc8376283cf038048c5c6b0d71b1e1932a1d6f1d
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-11 16:46:55 +0200

    Basic math

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,4 +1,18 @@
 fn main() {
-    const x: u32 = 5;
-    println!("The value of x is: {}", x);
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("The value of x is: {}", remainder);
 }
