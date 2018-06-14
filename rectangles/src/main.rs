25283a5efedea519c99db24a7a70f86547999eb1
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-14 11:10:37 +0200

    can_hold exercise

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -7,16 +7,25 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width > rect.width && self.height > rect.height
+    }
 }
 
 fn main() {
     let rect1 = Rectangle {
-        height: 30,
-        width: 50,
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
     };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area()
-    );
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
