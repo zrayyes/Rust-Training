a25ccf6d9fdf1cc9d91ae1fd40de55161debc206
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-14 11:12:53 +0200

    Associated Function

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -10,6 +10,12 @@ impl Rectangle {
     fn can_hold(&self, rect: &Rectangle) -> bool {
         self.width > rect.width && self.height > rect.height
     }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -28,4 +34,6 @@ fn main() {
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3); // Associated Function
 }
