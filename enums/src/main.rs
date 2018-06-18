0a0e25adc1560747eb334c0aac9ba372e5611d24
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-18 09:44:48 +0200

    Enum in struct

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,10 +3,25 @@ enum IpAddrKind {
     V6,
 }
 
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+    
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
-    println!("123");
+    println!("{}", home.address);
 }
 
 fn route(ip_type: IpAddrKind) {}
