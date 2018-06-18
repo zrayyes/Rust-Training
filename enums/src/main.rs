ec934124d3eb3526af6c228a9f77502d1c4f2a9f
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-18 09:48:19 +0200

    Enum variants with type associated

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,27 +1,14 @@
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddr::V4(String::from("127.0.0.1"));
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-    
+    let loopback = IpAddr::V6(String::from("::1"));
+
     route(IpAddrKind::V4);
     route(IpAddrKind::V6);
-    println!("{}", home.address);
+    println!("{}");
 }
-
-fn route(ip_type: IpAddrKind) {}
