536934452474a698c9cf9a9b9926eb3a2a2df79a
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-19 09:36:17 +0200

    Enum functions

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,14 +1,18 @@
-enum IpAddr {
-    V4(String),
-    V6(String),
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        // method body would be defined here
+    }
 }
 
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
-
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
-    println!("{}");
+    let m = Message::Write(String::from("hello"));
+    m.call();
+    println!("123");
 }
