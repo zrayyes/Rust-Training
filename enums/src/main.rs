64f54ccfea2c40cec618ef80317e65c76c118de4
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-19 09:51:47 +0200

    Match functions

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -37,7 +37,10 @@ fn main() {
 
 fn value_in_cents(coin: Coin) -> u32 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
