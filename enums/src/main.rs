c14662cee0198de323e5b978e1f9b019b63a14cb
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-19 10:00:20 +0200

    _ placeholder

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -45,6 +45,15 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (), // match any other value
+    }
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
