791b874ade0a8fa258427fbc614eda5400e1733e
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-21 11:50:10 +0200

    Added matcher

--- progress_calculator/src/main.rs
+++ progress_calculator/src/main.rs
@@ -5,9 +5,13 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
-        println!("Select Option: ");
-        println!("1 - Calculate percentage done.");
-        println!("2 - Calculate targeted percentage.");
+        welcome_message();
+
+        match readline().trim() {
+            "1" => println!("{}% done!", calculate_percentage()),
+            "2" => println!("two"),
+            _ => println!("Invalid option"),
+        }
     } else if args[1] == "1" {
         println!("{}% done!", calculate_percentage());
     } else {
@@ -15,15 +19,17 @@ fn main() {
     }
 }
 
+fn welcome_message() -> {
+        println!("Select Option: ");
+        println!("1 - Calculate percentage done.");
+        println!("2 - Calculate targeted percentage.");
+}
+
 fn calculate_percentage() -> f32 {
     let start = readline_as_float("Please input starting weight: ");
     let target = readline_as_float("Please input target weight: ");
     let current = readline_as_float("Please input current weight: ");
-    do_math(start, target, current) * 100.0
-}
-
-fn do_math(start: f32, target: f32, current: f32) -> f32 {
-    (start - current) / (start - target)
+    ((start - current) / (start - target)) * 100.0
 }
 
 fn readline() -> String {
