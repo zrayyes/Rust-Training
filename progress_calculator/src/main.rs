56dfcfd61c307cc6bf8cc1636c6e12d3a2a4d92e
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-21 11:11:48 +0200

    Added messages

--- progress_calculator/src/main.rs
+++ progress_calculator/src/main.rs
@@ -1,19 +1,21 @@
 use std::io;
 
 fn main() {
-    let start = readline_as_float();
-    let stop = readline_as_float();
-    let current = readline_as_float();
+    let start = readline_as_float("Please input starting weight: ");
+    let target = readline_as_float("Please input target weight: ");
+    let current = readline_as_float("Please input current weight: ");
 
-    println!("{}% done", do_math(start, stop, current) * 100.0);
+    println!("{}% done!", do_math(start, target, current) * 100.0);
 }
 
-fn do_math(start: f32, end: f32, current: f32) -> f32 {
-    (start - current) / (start - end)
+fn do_math(start: f32, target: f32, current: f32) -> f32 {
+    (start - current) / (start - target)
 }
 
-fn readline_as_float() -> f32 {
+fn readline_as_float(message: &str) -> f32 {
     let mut read = String::new();
+    
+    println!("{}", message);
 
     io::stdin()
         .read_line(&mut read)
