63c58ae0f901b06dbb7ab5007e202ade73ab7b24
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-21 11:02:33 +0200

    Added read line as float

--- progress_calculator/src/main.rs
+++ progress_calculator/src/main.rs
@@ -1,7 +1,23 @@
+use std::io;
+
 fn main() {
-    println!("{}% done",do_math(3.0, 1.0, 2.3)*100.0);
+    let start = readline_as_float();
+    let stop = readline_as_float();
+    let current = readline_as_float();
+
+    println!("{}% done", do_math(start, stop, current) * 100.0);
 }
 
 fn do_math(start: f32, end: f32, current: f32) -> f32 {
-    (start-current)/(start-end)
+    (start - current) / (start - end)
+}
+
+fn readline_as_float() -> f32 {
+    let mut read = String::new();
+
+    io::stdin()
+        .read_line(&mut read)
+        .expect("Failed to read line");
+    
+    read.trim().parse::<f32>().unwrap()
 }
