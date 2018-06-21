481261a9b5443c411c089c9b5da0aa38c3d1f86f
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-21 11:39:59 +0200

    Added argument read

--- progress_calculator/src/main.rs
+++ progress_calculator/src/main.rs
@@ -1,12 +1,23 @@
+use std::env;
 use std::io;
 use std::process;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if (args.len() == 1) {
+        println!("Normal");
+    } else if (args[1] == "1") {
+        println!("{}% done!", calculate_percentage());
+    } else {
+        println!("Invalid argument/s");
+    }
+}
+
+fn calculate_percentage() -> f32 {
     let start = readline_as_float("Please input starting weight: ");
     let target = readline_as_float("Please input target weight: ");
     let current = readline_as_float("Please input current weight: ");
-
-    println!("{}% done!", do_math(start, target, current) * 100.0);
+    do_math(start, target, current) * 100.0
 }
 
 fn do_math(start: f32, target: f32, current: f32) -> f32 {
