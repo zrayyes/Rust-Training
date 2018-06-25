f78182b45842041db3e793fcc40c1e630621489b
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-25 10:36:30 +0200

    strings add

--- collections/src/strings.rs
+++ collections/src/strings.rs
@@ -1,5 +1,11 @@
 fn main() {
     let mut s = String::from("initial contents");
-    s.push_str(" bar");
-    println!("{}", s);
+    let s2 = "bar";
+    s.push_str(s2);
+    println!("s2 is {}", s2);
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // Note s1 has been moved here and can no longer be used
+    println!("s3 is {}", s3);
 }
