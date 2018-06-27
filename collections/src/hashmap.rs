6a2cdc0849545585d73869fbd7ac481f2238b4f7
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-27 09:41:54 +0200

    vector to hashmap

--- collections/src/hashmap.rs
+++ collections/src/hashmap.rs
@@ -8,4 +8,12 @@ fn main() {
 
     println!("{:?}", scores);
     println!("{}", scores["Blue"]);
+
+    // OR
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let scores = vec![10, 50];
+
+    // _ - infer type from values
+    let scores: HashMap<_, _> = teams.iter().zip(scores.iter()).collect();
 }
