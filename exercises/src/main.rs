91008756e7823e0611d7b2929109a2c2d35718e4
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-27 10:45:46 +0200

    vector median

--- exercises/src/main.rs
+++ exercises/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    let mut list = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
-    println!("{:?}", mean(&list));
+    println!("Length: {}", list.len());
+    println!("Mean: {}", mean(&list));
+    println!("Median: {}", median(&mut list));
 }
 
 fn mean(list: &Vec<i32>) -> f32 {
@@ -10,4 +12,13 @@ fn mean(list: &Vec<i32>) -> f32 {
         count += number
     }
     (count as f32) / (list.len() as f32)
+}
+
+fn median(list: &mut Vec<i32>) -> f32 {
+    list.sort();
+    match list.len() % 2 {
+        0 => ((list[list.len() / 2] as f32) + (list[(list.len() / 2) - 1] as f32)) / 2.0,
+        1 => list[list.len() / 2] as f32,
+        _ => -1.0,
+    }
 }
