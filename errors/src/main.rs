79778c57541c17bcbcfff1a8fdcae670acb78c74
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-28 11:30:45 +0200

    propagating Errors

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,26 +1,29 @@
 use std::fs::File;
+use std::io;
 use std::io::ErrorKind;
+use std::io::Read;
 
 fn main() {
-    let f = File::open("hello.txt");
-
-    let f = match f {
-        Ok(file) => file,
-        //             match guard, check if error returned is 'NotFound'
-        // Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
-        //     Ok(fc) => fc,
-        //     Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
-        // },
-        Err(error) => {
-            panic!("There was a problem opening the file: {:?}", error);
-        }
-    };
-
-    // Shortcuts
-
     // Return value inside Ok if available, else panic!
     let g = File::open("text.txt").unwrap();
 
     // Similar to unwrap, but with panic! message
     let h = File::open("text.txt").expect("Failed to open text.txt");
+}
+
+// Returns a Result with concrete types String, and io::Error
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
