43a0ac945845587f62a1c711d30c0212dcc0c071
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-28 11:24:20 +0200

    unwrap and expect keywords

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -7,12 +7,20 @@ fn main() {
     let f = match f {
         Ok(file) => file,
         //             match guard, check if error returned is 'NotFound'
-        Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
-            Ok(fc) => fc,
-            Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
-        },
+        // Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
+        //     Ok(fc) => fc,
+        //     Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
+        // },
         Err(error) => {
             panic!("There was a problem opening the file: {:?}", error);
         }
     };
+
+    // Shortcuts
+
+    // Return value inside Ok if available, else panic!
+    let g = File::open("text.txt").unwrap();
+
+    // Similar to unwrap, but with panic! message
+    let h = File::open("text.txt").expect("Failed to open text.txt");
 }
