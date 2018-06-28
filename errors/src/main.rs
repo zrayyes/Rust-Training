4e39b9123c9aa5445dc2e20830788a87fcfab584
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-28 11:36:18 +0200

    ? keyword

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io;
-use std::io::ErrorKind;
 use std::io::Read;
 
 fn main() {
@@ -26,4 +25,12 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(s),
         Err(e) => Err(e),
     }
+}
+
+// same as above but shorter with ?
+fn read_username_shortcut() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
 }
