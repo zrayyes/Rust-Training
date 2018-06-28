d3016ad150a80cce02430c56d58fadd7002f68e3
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-28 11:12:52 +0200

    handling Result Enum

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+
 fn main() {
-    panic!("crash and burn");
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening the file: {:?}", error);
+        }
+    };
 }
