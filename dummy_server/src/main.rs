1aa8c3bf30e2f2621777bf46d42a2712841efa6a
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-06-29 10:11:58 +0200

    echo service created

--- dummy_server/src/main.rs
+++ dummy_server/src/main.rs
@@ -1,29 +1,44 @@
+extern crate futures;
 extern crate hyper;
 
+use futures::future;
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+
+type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
 fn main() {
-    // This is our socket address...
     let addr = ([127, 0, 0, 1], 3000).into();
 
-    // A `Service` is needed for every connection, so this
-    // creates on of our `hello_world` function.
-    let new_svc = || {
-        // service_fn_ok converts our function into a `Service`
-        service_fn_ok(hello_world)
-    };
+    let new_svc = || service_fn_ok(hello_world);
 
     let server = Server::bind(&addr)
         .serve(new_svc)
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Server running on http://{}/", addr);
-    // Run this server
     hyper::rt::run(server);
 }
 
 fn hello_world(_req: Request<Body>) -> Response<Body> {
     Response::new(Body::from("Hello, World!"))
+}
+
+fn echo(req: Request<Body>) -> BoxFut {
+    let mut response = Response::new(Body::empty());
+
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        }
+        (&Method::POST, "/echo") => {
+            *response.body_mut() = req.into_body();
+        }
+        _ => {
+            *response.status_mut() = StatusCode::NOT_FOUND;
+        }
+    };
+
+    Box::new(future::ok(response))
 }
