a58ebfa29cb3a669f75b54f25a6b45601f4a95ff
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-07-04 09:19:25 +0200

    largest number as function

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,13 +1,20 @@
 fn main() {
-    let number_list = vec![34,50,25,100,65];
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    let mut largest = number_list[0];
+    println!("The largest number is {}", largest(&number_list));
 
-    for number in number_list{
-        if number > largest {
-            largest = number;
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    println!("The largest number is {}", largest(&number_list));
+}
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
         }
     }
-
-    println!("Largest number is {}", largest);
+    largest
 }
