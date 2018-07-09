d29182cc4129b634f3d957457e898cf3082d3883
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-07-09 09:35:22 +0200

    Added multiple traits to generic type

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -52,7 +52,7 @@ fn main() {
 }
 
 // T stays the same type after being specified
-fn largest<T>(list: &[T]) -> T {
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
