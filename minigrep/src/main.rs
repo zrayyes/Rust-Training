bb855b7329567e588600428f46b624ff5c77480a
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-07-11 09:51:44 +0200

    added argument selection

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,14 @@
 use std::env;
 
 fn main() {
+    // .collect -> iterator to vector
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let path = &args[0];
+    let query= &args[1];
+    let filename= &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+    
 }
