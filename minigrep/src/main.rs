0adb5bcd1f08559c8be520869d270a2a6196a70f
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-07-12 09:23:06 +0200

    refactored args

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,9 +7,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 3 {
-        // let path = &args[0];
-        let query = &args[1];
-        let filename = &args[2];
+        let (query, filename) = parse_config(&args);
 
         println!("Searching for {}", query);
         println!("In file {}", filename);
@@ -17,11 +15,17 @@ fn main() {
         // ? -> handle error
         let mut f = File::open(filename).expect("file not found");
         let mut content = String::new();
-        f.read_to_string(&mut content)
-            .expect("failed to read file");
+        f.read_to_string(&mut content).expect("failed to read file");
 
         println!("Found: \n{}", content);
     } else {
         println!("Invalid number of arguments, need query and filename");
     }
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
