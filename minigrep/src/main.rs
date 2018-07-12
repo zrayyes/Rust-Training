e1cce8d6a42a88d99ef0b1a6659a7f861085f46c
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-07-12 14:58:40 +0200

    Added Config struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,18 +2,23 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
 fn main() {
     // .collect -> iterator to vector
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 3 {
-        let (query, filename) = parse_config(&args);
+        let config = parse_config(&args);
 
-        println!("Searching for {}", query);
-        println!("In file {}", filename);
+        println!("Searching for {}", config.query);
+        println!("In file {}", config.filename);
 
         // ? -> handle error
-        let mut f = File::open(filename).expect("file not found");
+        let mut f = File::open(config.filename).expect("file not found");
         let mut content = String::new();
         f.read_to_string(&mut content).expect("failed to read file");
 
@@ -23,9 +28,9 @@ fn main() {
     }
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
 
-    (query, filename)
+    Config { query, filename }
 }
