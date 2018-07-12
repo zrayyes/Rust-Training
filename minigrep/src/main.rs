871bdb47659cfb17bd9889174fc23beaa69c6068
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-07-12 15:01:26 +0200

    fixed args.len error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -11,21 +11,20 @@ fn main() {
     // .collect -> iterator to vector
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 3 {
-        let config = parse_config(&args);
-
-        println!("Searching for {}", config.query);
-        println!("In file {}", config.filename);
-
-        // ? -> handle error
-        let mut f = File::open(config.filename).expect("file not found");
-        let mut content = String::new();
-        f.read_to_string(&mut content).expect("failed to read file");
-
-        println!("Found: \n{}", content);
-    } else {
-        println!("Invalid number of arguments, need query and filename");
+    if args.len() < 3 {
+        panic!("Invalid number of arguments, need query and filename");
     }
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    // ? -> handle error
+    let mut f = File::open(config.filename).expect("file not found");
+    let mut content = String::new();
+    f.read_to_string(&mut content).expect("failed to read file");
+
+    println!("Found: \n{}", content);
 }
 
 fn parse_config(args: &[String]) -> Config {
