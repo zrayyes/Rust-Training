044cd2ff3b437f349ff77fc684cac1ca47958ffa
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-07-13 11:57:41 +0200

    moved all functions to external crate

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,31 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut content = String::new();
+    f.read_to_string(&mut content)?;
+
+    println!("Found: \n{}", content);
+
+    Ok(())
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,25 +1,9 @@
+extern crate minigrep;
+
+use minigrep::run;
+use minigrep::Config;
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            panic!("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
 
 fn main() {
     // .collect -> iterator to vector
@@ -38,15 +22,4 @@ fn main() {
 
         process::exit(1);
     }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut content = String::new();
-    f.read_to_string(&mut content)?;
-
-    println!("Found: \n{}", content);
-
-    Ok(())
 }
