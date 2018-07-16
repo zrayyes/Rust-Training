bf86705085d3286ad7dee0e17936349b528a94e4
Zuhair Rayyes <rayyes.z@ikarus.at>
2018-07-16 10:11:55 +0200

    Added dummy test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -25,7 +25,25 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
     let mut content = String::new();
     f.read_to_string(&mut content)?;
 
-    println!("Found: \n{}", content);
-
     Ok(())
+}
+
+pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let content = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, content));
+    }
 }
