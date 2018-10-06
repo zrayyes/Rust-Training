65ee440b3ec10f578d6ae3c497f859308fecb567
Zuhair Rayyes <zuz1995@yahoo.com>
2018-10-06 12:15:38 +0200

    Added pause

--- mem-test/src/main.rs
+++ mem-test/src/main.rs
@@ -5,6 +5,22 @@ fn main() {
     for name in process_names {
         println!("{}", name);
     }
+    pause();
+}
+
+fn pause() {
+    use std::io;
+    use std::io::prelude::*;
+
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "Press any key to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
 }
 
 fn get_processes() -> Vec<String> {
