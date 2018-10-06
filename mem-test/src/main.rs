9ecbd477ef551c5b67a736da20b02149c2b7077c
Zuhair Rayyes <zuz1995@yahoo.com>
2018-10-06 12:12:41 +0200

    print process name

--- mem-test/src/main.rs
+++ mem-test/src/main.rs
@@ -1,12 +1,20 @@
+// TODO: Add process struct with functions
+
 fn main() {
-    get_processes();
+    let process_names = get_processes();
+    for name in process_names {
+        println!("{}", name);
+    }
 }
 
-fn get_processes() {
+fn get_processes() -> Vec<String> {
+    use regex::Regex;
     use std::ffi::CString;
     use winapi::shared::minwindef::DWORD;
     use winapi::um::psapi;
+    let re = Regex::new(r"[^\\]*$").unwrap();
 
+    let mut processes = Vec::new();
     let mut bytes_returned: DWORD = 0;
     // Create a large array to contain all process ids
     let mut pids: [DWORD; 1024] = [0; 1024];
@@ -26,9 +34,11 @@ fn get_processes() {
                 let c_str = CString::from_raw(filename.as_mut_ptr());
                 let str_buf: String = c_str.to_str().unwrap().to_owned();
                 if str_buf != "" {
-                    println!("{}", str_buf);
+                    let str_cap = re.captures(&str_buf).unwrap();
+                    processes.push(str_cap.get(0).unwrap().as_str().to_owned());
                 }
             }
         }
     }
+    processes
 }
