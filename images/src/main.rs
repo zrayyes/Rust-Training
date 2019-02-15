6b4aff95cbb4967d6e9eaa8dc1b7995efbbed934
Zuhair Rayyes <rayyes.z@ikarus.at>
2019-02-15 12:52:03 +0100

    created image joiner

--- images/src/main.rs
+++ images/src/main.rs
@@ -1,20 +1,20 @@
 extern crate image;
-
 use image::{GenericImageView, ImageBuffer};
+use std::fs;
 
 fn main() {
-    let boy = image::open("boy.png").unwrap();
-    let girl = image::open("girl.png").unwrap();
-
-    let mut imgbuf = ImageBuffer::new(boy.width(), boy.height());
-
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        if (x + y) % 2 == 0 {
-            *pixel = boy.get_pixel(x, y);
-        } else {
-            *pixel = girl.get_pixel(x, y);
-        }
+    let mut imgs = vec![];
+    let paths = fs::read_dir("./img").unwrap();
+    for path in paths {
+        imgs.push(image::open(path.unwrap().path()).unwrap());
     }
+    if imgs.len() > 0 {
+        let mut imgbuf = ImageBuffer::new(imgs[0].width(), imgs[0].height());
 
-    imgbuf.save("join.png").unwrap();
+        for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+            *pixel = imgs[((x + y) % imgs.len() as u32) as usize].get_pixel(x, y);
+        }
+
+        imgbuf.save("join.png").unwrap();
+    }
 }
