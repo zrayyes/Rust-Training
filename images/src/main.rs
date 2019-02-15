ca467fe58fb6d927d0a9aa650d59161876725b00
Zuhair Rayyes <rayyes.z@ikarus.at>
2019-02-15 13:00:15 +0100

    Added a height and width check

--- images/src/main.rs
+++ images/src/main.rs
@@ -12,7 +12,10 @@ fn main() {
         let mut imgbuf = ImageBuffer::new(imgs[0].width(), imgs[0].height());
 
         for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-            *pixel = imgs[((x + y) % imgs.len() as u32) as usize].get_pixel(x, y);
+            let imgg = &imgs[((x + y) % imgs.len() as u32) as usize];
+            if imgg.width() == imgs[0].width() && imgg.height() == imgs[0].height() {
+                *pixel = imgg.get_pixel(x, y);
+            }
         }
 
         imgbuf.save("join.png").unwrap();
