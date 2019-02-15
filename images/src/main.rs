062c81356a3f00dbefb5ddb97f6518d1fe3237dc
Zuhair Rayyes <rayyes95@gmail.com>
2019-02-15 18:15:36 +0100

    Added check for empty pixels

--- images/src/main.rs
+++ images/src/main.rs
@@ -14,7 +14,13 @@ fn main() {
         for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
             let imgg = &imgs[((x + y) % imgs.len() as u32) as usize];
             if imgg.width() == imgs[0].width() && imgg.height() == imgs[0].height() {
-                *pixel = imgg.get_pixel(x, y);
+                let pix = imgg.get_pixel(x, y);
+                if (pix[0]as u64 + pix[1]as u64 + pix[2]as u64 + pix[3]as u64 ) == 0 {
+                    *pixel = imgs[0].get_pixel(x, y);
+                } else {
+                    *pixel = pix;
+                }
+                
             }
         }
 
