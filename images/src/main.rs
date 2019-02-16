f60c6c2a013a50e773a7f0beb9424f7a02e2387e
Zuhair Rayyes <rayyes95@gmail.com>
2019-02-16 22:51:12 +0100

    Added pixel randomizer

--- images/src/main.rs
+++ images/src/main.rs
@@ -1,5 +1,6 @@
 extern crate image;
 use image::{DynamicImage, GenericImageView, ImageBuffer, RgbaImage};
+use rand::Rng;
 use std::fs;
 
 fn main() {
@@ -11,6 +12,9 @@ fn main() {
     if imgs.len() > 0 {
         let joined = join(&imgs);
         joined.save("join.png").unwrap();
+
+        let random = randomize(&imgs);
+        random.save("random.png").unwrap();
     }
 }
 
@@ -18,6 +22,23 @@ fn join(images: &Vec<DynamicImage>) -> RgbaImage {
     let mut buffer = ImageBuffer::new(images[0].width(), images[0].height());
     for (x, y, pixel) in buffer.enumerate_pixels_mut() {
         let imgg = &images[((x + y) % images.len() as u32) as usize];
+        if imgg.width() == images[0].width() && imgg.height() == images[0].height() {
+            let pix = imgg.get_pixel(x, y);
+            if (pix[0] as u64 + pix[1] as u64 + pix[2] as u64 + pix[3] as u64) == 0 {
+                *pixel = images[0].get_pixel(x, y);
+            } else {
+                *pixel = pix;
+            }
+        }
+    }
+    buffer
+}
+
+fn randomize(images: &Vec<DynamicImage>) -> RgbaImage {
+    let mut buffer = ImageBuffer::new(images[0].width(), images[0].height());
+    let mut rng = rand::thread_rng();
+    for (x, y, pixel) in buffer.enumerate_pixels_mut() {
+        let imgg = &images[(rng.gen_range(0, images.len())) as usize];
         if imgg.width() == images[0].width() && imgg.height() == images[0].height() {
             let pix = imgg.get_pixel(x, y);
             if (pix[0] as u64 + pix[1] as u64 + pix[2] as u64 + pix[3] as u64) == 0 {
