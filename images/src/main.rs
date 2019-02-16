89ab9d40ab078bdd891eceae4cdaa9ee7db342ec
Zuhair Rayyes <rayyes95@gmail.com>
2019-02-16 22:44:12 +0100

    added join function

--- images/src/main.rs
+++ images/src/main.rs
@@ -1,5 +1,5 @@
 extern crate image;
-use image::{GenericImageView, ImageBuffer};
+use image::{DynamicImage, GenericImageView, ImageBuffer, RgbaImage};
 use std::fs;
 
 fn main() {
@@ -9,21 +9,23 @@ fn main() {
         imgs.push(image::open(path.unwrap().path()).unwrap());
     }
     if imgs.len() > 0 {
-        let mut imgbuf = ImageBuffer::new(imgs[0].width(), imgs[0].height());
+        let joined = join(&imgs);
+        joined.save("join.png").unwrap();
+    }
+}
 
-        for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-            let imgg = &imgs[((x + y) % imgs.len() as u32) as usize];
-            if imgg.width() == imgs[0].width() && imgg.height() == imgs[0].height() {
-                let pix = imgg.get_pixel(x, y);
-                if (pix[0]as u64 + pix[1]as u64 + pix[2]as u64 + pix[3]as u64 ) == 0 {
-                    *pixel = imgs[0].get_pixel(x, y);
-                } else {
-                    *pixel = pix;
-                }
-                
+fn join(images: &Vec<DynamicImage>) -> RgbaImage {
+    let mut buffer = ImageBuffer::new(images[0].width(), images[0].height());
+    for (x, y, pixel) in buffer.enumerate_pixels_mut() {
+        let imgg = &images[((x + y) % images.len() as u32) as usize];
+        if imgg.width() == images[0].width() && imgg.height() == images[0].height() {
+            let pix = imgg.get_pixel(x, y);
+            if (pix[0] as u64 + pix[1] as u64 + pix[2] as u64 + pix[3] as u64) == 0 {
+                *pixel = images[0].get_pixel(x, y);
+            } else {
+                *pixel = pix;
             }
         }
-
-        imgbuf.save("join.png").unwrap();
     }
+    buffer
 }
